a9ce5eff326d46c1c163c77fe66adee9104723a3
zenbread <nmcamp87@gmail.com>
2017-06-08 10:29:33 -0400

    Create NmapServiceParse.sh

--- NmapServiceParse.sh
+++ NmapServiceParse.sh
@@ -2,12 +2,12 @@
 
 
 
-#First Part of the problem.
-#cat NMAP_all_hosts.txt |  grep open | grep -vE 'OSScan' | awk '{print $3}' | sort | uniq -c | sort -rn
+#Gives you the services and how many systems are running them.
+cat NMAP_all_hosts.txt |  grep open | grep -vE 'OSScan' | awk '{print $3}' | sort | uniq -c | sort -rn
 
 
+#Pull services with IP's running them.
 services=$(cat NMAP_all_hosts.txt | grep open | grep -vE 'OSScan' | awk '{print $3}' | sort -u)
-
 
 for x in $services; do
 	a=$(cat NMAP_all_hosts.txt |awk '/Nmap scan/,/MAC Add/' | grep -v "Host\|PORT\|Not\|MAC" | awk '{print $NF}' | sed '/192/ i \\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n' | grep -B 18 $x | \
